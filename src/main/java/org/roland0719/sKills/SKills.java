202322b83ec3f8d002deb3b73005c4cd2e500061
onlyaapple <minecraft.roli.1000@gmail.com>
2026-05-30 12:49:23 +0200

    Update SKills.java

--- src/main/java/org/roland0719/sKills/SKills.java
+++ src/main/java/org/roland0719/sKills/SKills.java
@@ -3,83 +3,73 @@ package org.roland0719.sKills;
 import java.io.File;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.roland0719.sKills.command.SKillsCommand;
+import org.roland0719.sKills.listeners.DeathLighting;
+import org.roland0719.sKills.listeners.DeathReward;
+import org.roland0719.sKills.listeners.DeathMessage;
+import org.roland0719.sKills.placeholders.SKillsPlaceholders;
 import org.roland0719.sKills.database.DatabaseManager;
 import org.roland0719.sKills.database.PlayerStatsDAO;
-import org.roland0719.sKills.listeners.DeathMessage;
-import org.roland0719.sKills.listeners.DeathReward;
-import org.roland0719.sKills.placeholders.SKillsPlaceholders;
+import org.roland0719.sKills.command.SKillsCommand;
 import org.roland0719.sKills.utils.UpdateChecker;
 import org.roland0719.sKills.utils.UpdateJoinListener;
 
 public final class SKills extends JavaPlugin implements Listener {
-   private FileConfiguration messages;
-   private File messagesFile;
-   private DatabaseManager database;
-   private PlayerStatsDAO stats;
+    private FileConfiguration messages;
+    private File messagesFile;
+    private DatabaseManager database;
+    private PlayerStatsDAO stats;
+    private SKillsCommand killsCommand;
+    private DeathMessage deathMessage;
 
-   public void onEnable() {
-      this.saveDefaultMessages();
-      this.saveDefaultConfig();
-      FileConfiguration config = this.getConfig();
-      this.database = new DatabaseManager(this);
-      this.stats = new PlayerStatsDAO(this.database);
-      this.getServer().getPluginManager().registerEvents(this, this);
-      this.getServer().getPluginManager().registerEvents(new DeathMessage(this, this.messages), this);
-      this.getServer().getPluginManager().registerEvents(new DeathReward(config), this);
-      if (this.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
-         (new SKillsPlaceholders(this)).register();
-      }
+    public void onEnable() {
+        this.saveDefaultMessages();
+        saveDefaultConfig();
+        FileConfiguration config = this.getConfig();
+        database = new DatabaseManager(this);
+        stats = new PlayerStatsDAO(database);
+        deathMessage = new DeathMessage(this, this.messages);
+        this.getServer().getPluginManager().registerEvents(deathMessage, this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+        this.getServer().getPluginManager().registerEvents(new DeathReward(this), this);
+        this.getServer().getPluginManager().registerEvents(new DeathLighting(this, messages), this);
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new SKillsPlaceholders(this).register();
+        }
+        UpdateChecker updateChecker = new UpdateChecker(this, "Roland0719/SKills");
+        this.getServer().getPluginManager().registerEvents(new UpdateJoinListener(this, updateChecker), this);
+        if (getConfig().getBoolean("update-checker", true)) { updateChecker.checkForUpdates(); }
+        killsCommand = new SKillsCommand(this);
+        this.getCommand("skills").setExecutor(killsCommand);
+        this.getCommand("skills").setTabCompleter(killsCommand);
+        this.getLogger().info("SKills » The plugin successfully started.");
+    }
 
-      UpdateChecker updateChecker = new UpdateChecker(this, "Roland0719/SKills");
-      this.getServer().getPluginManager().registerEvents(new UpdateJoinListener(this, updateChecker), this);
-      if (this.getConfig().getBoolean("update-checker", true)) {
-         updateChecker.checkForUpdates();
-      }
+    public void onDisable() {
+        this.getLogger().info("SKills » Plugin disabled.");
+    }
 
-      SKillsCommand KillsCommand = new SKillsCommand(this);
-      this.getCommand("skills").setExecutor(KillsCommand);
-      this.getCommand("skills").setTabCompleter(KillsCommand);
-      this.getLogger().info("SKills » The plugin successfully started.");
-   }
+    private void saveDefaultMessages() {
+        if (!this.getDataFolder().exists()) {
+            this.getDataFolder().mkdirs();
+        }
 
-   public void onDisable() {
-      this.getLogger().info("SKills » Plugin disabled.");
-   }
+        this.messagesFile = new File(this.getDataFolder(), "messages.yml");
+        if (!this.messagesFile.exists()) {
+            this.saveResource("messages.yml", false);
+        }
 
-   private void saveDefaultMessages() {
-      if (!this.getDataFolder().exists()) {
-         this.getDataFolder().mkdirs();
-      }
-
-      this.messagesFile = new File(this.getDataFolder(), "messages.yml");
-      if (!this.messagesFile.exists()) {
-         this.saveResource("messages.yml", false);
-      }
-
-      this.messages = YamlConfiguration.loadConfiguration(this.messagesFile);
-   }
-
-   public void reloadMessages() {
-      this.messages = YamlConfiguration.loadConfiguration(this.messagesFile);
-   }
-
-   public FileConfiguration getMessages() {
-      return this.messages;
-   }
-
-   public int getKills(Player p) {
-      return this.stats.getKills(p.getUniqueId());
-   }
-
-   public int getDeaths(Player p) {
-      return this.stats.getDeaths(p.getUniqueId());
-   }
-
-   public PlayerStatsDAO getStats() {
-      return this.stats;
-   }
+        this.messages = YamlConfiguration.loadConfiguration(this.messagesFile);
+    }
+    public void reloadMessages() {
+        this.messages = YamlConfiguration.loadConfiguration(this.messagesFile);
+        deathMessage.reload(this.messages);
+    }
+    public FileConfiguration getMessages() {
+        return this.messages;
+    }
+    public PlayerStatsDAO getStats() {
+        return stats;
+    }
 }
